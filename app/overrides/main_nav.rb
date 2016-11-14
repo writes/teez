3841fb3bb5bae653ed9e6ea5c763644f8ca6eae4
jona8578 <jonathon.thompson@rackspace.com>
2016-11-14 01:01:12 -0600

    remove additional level of nav and combine main and top

--- app/overrides/main_nav.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  :virtual_path => 'spree/shared/_main_nav_bar',
-  :name => 'main_nav_bar',
-  :insert_bottom => 'ul.nav.navbar-nav.navbar-right',
-  :partial => 'partials/menu'
-)
